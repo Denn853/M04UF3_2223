deedd2f12849b5c1ddbc72ff9602ffe854b63619
Dennys Hernández Muñiz <dennys.hernandez@enti.cat>
2023-02-08 14:43:38 +0100

    hay enemigos en pantalla y el player se mueve con las flechitas wiiiiii

--- index.js
+++ index.js
@@ -2,7 +2,51 @@
 const http = require("http");
 const fs = require("fs");
 
+function send_index(response) {
+	fs.readFile("index.html", function(err, data) {
+
+      if (err) {
+         console.error(err);
+         return;
+      }
+
+      response.writeHead(200, {"Content-Type":"text/html"});
+      response.write(data);
+
+      response.end();
+   });
+}
+
+function send_player(response) {
+	fs.readFile("player.png", function(err, data) {
+
+      if (err) {
+         console.error(err);
+         return;
+      }
+
+      response.writeHead(200, {"Content-Type":"image/png"});
+      response.write(data);
+
+      response.end();
+   });
+}
+
 http.createServer(function(request, response) {
+
+	console.log(request.url);
+
+	let url = request.url.split("/");
+
+	switch (url[1]) {
+		case "player.png":
+			send_player(response);			
+			break;
+
+		default:
+			send_index(response);
+
+	}
 
 	fs.readFile("index.html", function(err, data) {
 		
@@ -15,18 +59,6 @@ http.createServer(function(request, response) {
 		response.write(data);
 
 		response.end();
-	})
+	});
 
-	 fs.readFile("player.png", function(err, data) {
-
-      if (err) {
-         console.error(err);
-         return;
-      }
-
-      response.writeHead(200, {"Content-Type":"text/html"});
-      response.write(data);
-
-      response.end();
-   })
-})
+}).listen(8530);
